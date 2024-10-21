e88d20cca06e2fbe83b87e6a3021b059a3ef5d43
Shravastee Thakur <shravastee@gmail.com>
2024-10-21 21:58:09 +0530

    Admin Dashboard created

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -17,6 +17,39 @@ const TaskList = () => {
           sit, tempore tempora eum numquam?
         </p>
       </div>
+      <div className="h-full w-[300px] p-5 flex-shrink-0 bg-blue-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+          <h4>20 feb 2020</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Write assignments</h2>
+        <p className="mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime optio
+          sit, tempore tempora eum numquam?
+        </p>
+      </div>
+      <div className="h-full w-[300px] p-5 flex-shrink-0 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+          <h4>20 feb 2020</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Write assignments</h2>
+        <p className="mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime optio
+          sit, tempore tempora eum numquam?
+        </p>
+      </div>
+      <div className="h-full w-[300px] p-5 flex-shrink-0 bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+          <h4>20 feb 2020</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Write assignments</h2>
+        <p className="mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime optio
+          sit, tempore tempora eum numquam?
+        </p>
+      </div>
     </div>
   );
 };
