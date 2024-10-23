570812c8cd290c83f3ad4f10dd36be5cc5c38521
Shravastee Thakur <shravastee@gmail.com>
2024-10-23 22:14:28 +0530

    Local storage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./components/Auth/Login";
-import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
-import AdminDashboard from "./components/Dashboard/AdminDashboard";
-
+// import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+// import AdminDashboard from "./components/Dashboard/AdminDashboard";
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (email, password) => {
+    if (email == "admin@example.com" && password == "123") {
+      console.log("This is admin");
+    } else if (email == "employee@example.com" && password == "123") {
+      console.log("This is employee");
+    } else {
+      alert("Wrong email or password");
+    }
+  };
+
   return (
     <>
-      <Login />
+      {!user ? <Login handleLogin={handleLogin} /> : " "}
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -194,3 +194,13 @@ const admin = [
     password: "123",
   },
 ];
+
+export const setLocalStorage = () => {
+  localStorage.setItem("employees", JSON.stringify(employees));
+  localStorage.setItem("admin", JSON.stringify(admin));
+};
+
+export const getLocalStorage = () => {
+  const employees = JSON.parse(localStorage.getItem("employees"));
+  const admin = JSON.parse(localStorage.getItem("admin"));
+};
